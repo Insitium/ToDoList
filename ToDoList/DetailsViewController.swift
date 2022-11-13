db34faa256048a18e3d1bc82bc20354b53de0978
Sarthak Vashistha <vashistha.sarthak31@gmail.com>
2022-11-13 08:25:37 -0500

    navigation controller and first page

---
+++ ToDoList/DetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsViewController.swift
+//  ToDoList
+//
+//  Created by Sarthak Vashistha on 2022-11-13.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Todo Name Details"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
