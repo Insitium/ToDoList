1aaa93f172e366e6c1faf15ca747aedad7ed9669
Sarthak Vashistha <vashistha.sarthak31@gmail.com>
2022-11-13 22:23:42 -0500

    working code almost final

---
+++ ToDoList/ToDoCell.swift
@@ -0,0 +1,29 @@
+//
+//  ToDoCell.swift
+//  ToDoList
+//
+//  Created by Sarthak Vashistha on 2022-11-13.
+//
+
+import UIKit
+
+class ToDoCell: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var task: [String] = ["Task#1", "Task#2", "Task#3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Todo App"
         // Do any additional setup after loading the view.
     }
 
-
+   
+    @IBOutlet weak var table: UITableView!
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return task.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+                
+                cell.textLabel?.text = task[indexPath.row]
+                
+                let switchView = UISwitch(frame: .zero)
+                switchView.setOn(false, animated: true)
+       
+                switchView.tag = indexPath.row
+                switchView.addTarget(self, action: #selector(self.switchDidChange(_:)), for: .valueChanged)
+                cell.accessoryView = switchView
+                
+                
+                
+                return cell
+    }
+    @objc func switchDidChange(_ sender: UISwitch){
+        print("sender is \(sender.tag)")
+    }
 }
 
